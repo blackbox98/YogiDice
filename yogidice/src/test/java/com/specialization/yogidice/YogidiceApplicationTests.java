05007abb7c418697881c127fc875ed87237d4dc6
SSAFY 7기 전병찬 <81104902+blackbox98@users.noreply.github.com>
2023-04-12 22:20:48 +0900

    Add files via upload

---
+++ yogidice/src/test/java/com/specialization/yogidice/YogidiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.specialization.yogidice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogidiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
